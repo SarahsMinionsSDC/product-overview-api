3956cfd3d7134b32eaf4ed54fcc720e0c8d4a698
Michael Banuelos <mbanuelos1995@gmail.com>
2021-03-16 12:01:53 -0700

    updates to the mongoose schemas. looking into how to bulk import massive data

--- server/database/db.js
+++ server/database/db.js
@@ -6,3 +6,47 @@ await mongoose.connect('mongodb://localhost/sdc', {
   useFindAndModify: false,
   useCreateIndex: true
 });
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const essentialInfo = new Schema({
+  _id: ObjectId,
+  id: String,
+  name: String,
+  slogan: String,
+  description: String,
+  category: String,
+  default_price: String,
+});
+
+mongoose.model('essentialInfo', essentialInfo);
+
+const allInfo = new Schema({
+    features: [
+      {
+        feature: String,
+        value: String,
+      }
+    ],
+    results: [
+      {
+        name: String,
+        original_price: String,
+        sale_price: String,
+        default: Boolean,
+        photos: [
+          {
+            thumnail_url: String,
+            url: String,
+          }
+        ],
+        skus: {
+          number: {
+            quantity: Number,
+            size: String,
+          }
+        }
+      }
+    ]
+})
