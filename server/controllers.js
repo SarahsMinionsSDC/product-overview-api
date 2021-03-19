f42f105c18bd19ef3f795852436bee209d4651ec
Michael Banuelos <mbanuelos1995@gmail.com>
2021-03-18 19:35:26 -0700

    got docker to work. need to figure out how to get stuff on the cloud

--- server/controllers.js
+++ server/controllers.js
@@ -1,61 +1,58 @@
-const dbHelpers = require('./database/dbHelpers.js');
+const dbHelpers = require("./database/dbHelpers.js");
 
 const controllers = {
-    allProducts: (req, res) => {
-        dbHelpers.allProducts(req, (err, data) => {
-          if (err) {
-              res.status(400).send(err);
-          } else {
-              res.status(200).send(data);
-          }
-        })
-    },
-    getProduct: (req, res) => {
-        dbHelpers.getProduct(req, (err, data) => {
-          data = data.toJSON()
-          let formattedData = {
-            id: data.product_id,
-            name: data.name,
-            slogan: data.slogan,
-            description: data.description,
-            category: data.category,
-            default_price: data.default_price,
-            features: data.features
-          }
-          if (err) {
-            res.status(400).send(err);
-          } else {
-            res.status(200).send(formattedData);
-          }
-        })
-    },
-    getStyles: (req, res) => {
-        dbHelpers.getStyles(req, (err, data) => {
-          data = data.toJSON();
-          let formattedData = {
-            product_id: data.product_id,
-            results: data.results,
-          }
-            if (err) {
-                res.status(400).send(err);
-            } else {
-                res.status(200).send(formattedData);
-            }
-          })
-    },
-    getRelated: (req, res) => {
-        dbHelpers.getRelated(req, (err, data) => {
-          data = data.toJSON();
-          if (err) {
-              res.status(400).send(err);
-          }  else {
-              res.status(200).send(data.related_products);
-          }
-        })
-    },
-}
-
-
-
+  allProducts: (req, res) => {
+    dbHelpers.allProducts(req, (err, data) => {
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.status(200).send(data);
+      }
+    });
+  },
+  getProduct: (req, res) => {
+    dbHelpers.getProduct(req, (err, data) => {
+      data = data.toJSON();
+      let formattedData = {
+        id: data.product_id,
+        name: data.name,
+        slogan: data.slogan,
+        description: data.description,
+        category: data.category,
+        default_price: data.default_price,
+        features: data.features,
+      };
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.status(200).send(formattedData);
+      }
+    });
+  },
+  getStyles: (req, res) => {
+    dbHelpers.getStyles(req, (err, data) => {
+      data = data.toJSON();
+      let formattedData = {
+        product_id: data.product_id,
+        results: data.results,
+      };
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.status(200).send(formattedData);
+      }
+    });
+  },
+  getRelated: (req, res) => {
+    dbHelpers.getRelated(req, (err, data) => {
+      data = data.toJSON();
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.status(200).send(data.related_products);
+      }
+    });
+  },
+};
 
 module.exports = controllers;
--- server/database/db.js
+++ server/database/db.js
@@ -1,63 +1,66 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-mongoose.connect('mongodb://localhost/sdc', {useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connection.on('connected', function () {  
-  console.log('Mongoose default connection open to sdc');
-}); 
-
+mongoose.connect("mongodb://172.17.0.2:27017/tcp/sdc", {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", function () {
+  console.log("Mongoose default connection open to sdc");
+});
 
 const Schema = mongoose.Schema;
 
-const essentialInfo = new Schema({
-  _id: mongoose.Types.ObjectId,
-  id: String,
-  name: String,
-  slogan: String,
-  description: String,
-  category: String,
-  default_price: String,
-}, { strict: false });
-
+const essentialInfo = new Schema(
+  {
+    _id: mongoose.Types.ObjectId,
+    id: String,
+    name: String,
+    slogan: String,
+    description: String,
+    category: String,
+    default_price: String,
+  },
+  { strict: false }
+);
 
 const allInfo = new Schema({
   _id: mongoose.Types.ObjectId,
-  product_id: {type: String, index: true},
-    features: [
-      {
-        feature: String,
-        value: String,
-      }
-    ],
-    results: [
-      {
-        style_id: {type: String, index: true},
-        name: String,
-        original_price: String,
-        sale_price: String,
-        default: Number,
-        photos: [
-          {
-            thumnail_url: String,
-            url: String,
-          }
-        ],
-        skus: {
-          number: {
-            quantity: Number,
-            size: String,
-          }
-        }
-      }
-    ],
+  product_id: { type: String, index: true },
+  features: [
+    {
+      feature: String,
+      value: String,
+    },
+  ],
+  results: [
+    {
+      style_id: { type: String, index: true },
+      name: String,
+      original_price: String,
+      sale_price: String,
+      default: Number,
+      photos: [
+        {
+          thumnail_url: String,
+          url: String,
+        },
+      ],
+      skus: {
+        number: {
+          quantity: Number,
+          size: String,
+        },
+      },
+    },
+  ],
   related_products: Array,
-})
-allInfo.index({product_id: 1});
-allInfo.index({style_id: 1});
+});
+allInfo.index({ product_id: 1 });
+allInfo.index({ style_id: 1 });
 
-const product = mongoose.model('product', essentialInfo);
-const productInformation = mongoose.model('product-information', allInfo)
-
+const product = mongoose.model("product", essentialInfo);
+const productInformation = mongoose.model("product-information", allInfo);
 
 module.exports = {
   product: product,
